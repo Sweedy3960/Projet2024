7d72c5c35b6c2996a251169e604e57eb8701732c
aymeric3960 <pk38ubw@eduvaud.ch>
2024-05-23 13:31:10 +0200

    lala

---
+++ Project2024/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+
+void convdecbin(void)
+{
+	//Demander une valeur comprise entre 0 et 255
+	printf_s("Entrez une valeur decimale comprise entre 0 et 255 : ");
+	scanf_s("%d", &ValDec_initial);
+
+	
+}
+
+
+
+int ValDec_initial = 0; 
+void main(void)
+{
+
+
+}
